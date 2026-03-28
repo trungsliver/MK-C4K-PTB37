ee56400b0fa0e7b16a27b0977682920db2690cbd
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-03-28 18:57:26 +0700

    operators

---
+++ Lesson1/operators.py
@@ -0,0 +1,34 @@
+# Basic: + - * /
+    # Plus: +
+print('1 + 1 =', 1+1)
+
+    # Minus: -
+print('7 - 2 =', 7-2)
+
+    # Multiply: *
+print('3 * 4 =', 3*4)
+
+    # Divide: /
+print('10 / 2 =', 10/2)
+
+# Advanced: // % **
+    # Floor Division (chia lấy nguyên): //
+print('7 // 3 =', 7//3)
+
+    # Modulo (chia lấy dư): %
+print('7 % 3 =', 7%3)
+
+    # Exponentiation (lũy thừa): **
+print('2 ** 3 =', 2**3)
+
+    # square root (căn bậc 2)
+print('Square root of 16 =', 16**0.5)
+
+import math 
+print('Square root of 25 =', math.sqrt(25))
+
+# Test
+print('PI =', math.pi)
+
+    # round (làm tròn)
+print('PI =', round(math.pi, 2))
