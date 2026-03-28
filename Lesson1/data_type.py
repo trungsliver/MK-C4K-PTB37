f2933668b5c866c9456e0947a7c988f89365d673
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-03-28 18:57:11 +0700

    data types

---
+++ Lesson1/data_type.py
@@ -0,0 +1,20 @@
+# Data Types - Kiểu dữ liệu
+    # 1. String - chuỗi / xâu ký tự
+name = 'Duc Trung'
+address = "Ha Noi"
+
+    # 2. Integer - số nguyên
+age = 2
+
+    # 3. Float - số thực (real number)
+score = 9.5
+
+    # 4. Bool / Boolean - logic (True/False)
+is_student = True
+is_female = False
+
+# Check Type - Kiểm tra kiểu dữ liệu
+print('Type of name:', type(name))
+print('Type of age:', type(age))
+print('Type of score:', type(score))
+print('Type of is_student:', type(is_student))
