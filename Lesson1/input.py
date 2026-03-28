bebc7d53af1f20e52d111b595f75348e158eeb04
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-03-28 18:57:18 +0700

    input

---
+++ Lesson1/input.py
@@ -0,0 +1,12 @@
+# Input - Nhập dữ liệu từ bàn phím
+    # Default: string
+score = input('Enter your score: ')
+print('Type of score:', type(score))
+
+    # Convert to integer
+score2 = int(input('Enter your score2: '))
+print('Type of score2:', type(score2))
+
+    # Convert to float
+score3 = float(input('Enter your score3: '))
+print('Type of score3:', type(score3))
