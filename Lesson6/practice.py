a900294085d82fc5348a4f29af967c578b44a7b4
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-05-16 18:31:12 +0700

    Lesson 6

---
+++ Lesson6/practice.py
@@ -0,0 +1,15 @@
+# Bài 1: Nhập họ tên từ bàn phím. 
+# Chuẩn hóa họ tên: Xóa khoảng trắng dư thừa, viết hoa chữ cái đầu mỗi từ
+
+# Bài 2: Nhập vào 1 xâu ký tự định dạng dd/mm/yyyy (01/08/2024)
+    # Tách ngày, tháng, năm và hiển thị ra màn hình
+
+# Bài 3: Nhập vào một chuỗi bất kỳ.
+    # Đếm xem trong chuỗi có bao nhiêu ký tự đặc biệt.
+    # Biết ký tự đặc biệt là ký tự không phải chữ cái, số, khoảng trắng
+
+# Bài 4: Nhập vào một câu. Tìm từ có độ dài lớn nhất.
+    # Nếu có nhiều từ cùng độ dài, in ra từ xuất hiện đầu tiên.
+
+# Bài 5: Nhập vào một chuỗi.
+    # Xóa các ký tự bị lặp, chỉ giữ lần xuất hiện đầu tiên.
